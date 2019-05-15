8de19b9deaaafb5f97442f24fd58d5c3aba4b483
Santiago Zanella-Beguelin <santiago@microsoft.com>
2019-05-15 16:06:33 +0100

    Revert to upstream libsodium module

--- modules/libsodium/module.h
+++ modules/libsodium/module.h
@@ -10,9 +10,15 @@ class libsodium : public Module {
     private:
         std::optional<component::Digest> SHA256(operation::Digest& op) const;
         std::optional<component::Digest> SHA512(operation::Digest& op) const;
+        std::optional<component::MAC> HMAC_SHA256(operation::HMAC& op) const;
+        std::optional<component::MAC> HMAC_SHA512(operation::HMAC& op) const;
+        std::optional<component::MAC> HMAC_SHA512256(operation::HMAC& op) const;
     public:
         libsodium(void);
         std::optional<component::Digest> OpDigest(operation::Digest& op) override;
+        std::optional<component::MAC> OpHMAC(operation::HMAC& op) override;
+        std::optional<component::Ciphertext> OpSymmetricEncrypt(operation::SymmetricEncrypt& op) override;
+        std::optional<component::Cleartext> OpSymmetricDecrypt(operation::SymmetricDecrypt& op) override;
 };
 
 } /* namespace module */
